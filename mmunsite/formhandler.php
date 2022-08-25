c371305f684ce0472700e40df571197f910fa1cf
Francisco-MEB <105992395+Francisco-MEB@users.noreply.github.com>
2022-08-25 18:13:50 +0800

    Add files via upload
    Updated

--- mmunsite/formhandler.php
+++ mmunsite/formhandler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'mmun@website.com';
+$email_from = 'manilamun@website.com';
 
 $email_subject = 'Inquiry';
 
